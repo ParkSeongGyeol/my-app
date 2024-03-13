4ebb98c802d7076d6263432613c107fb216eed4d
ParkSeongGyeol <153297616+ParkSeongGyeol@users.noreply.github.com>
2024-03-13 14:28:27 +0900

    Create JavaScript.js

---
+++ JavaScript.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(3000, () => {
+  console.log('서버 실행 중...');
+});
